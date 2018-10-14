049d68d03e35f3c2a517c5ce878fe4135f907874
Catherine Salazar <catcreates13@gmail.com>
2018-10-14 23:25:39 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
