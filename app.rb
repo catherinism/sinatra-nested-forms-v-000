9576e87e8fd447d81c11cbfd3b0a21ceb633ab89
Catherine Salazar <catcreates13@gmail.com>
2018-10-15 00:05:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,13 +2,12 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
     get '/' do
       erb :root
     end
 
     get '/new' do
-      erb :"pirates/new"
+      erb :'pirates/new'
     end
 
     post '/pirates' do
@@ -18,8 +17,9 @@ module FormsLab
         Ship.new(details)
       end
       @ships = Ship.all
-      erb :"pirates/show"
-    end
 
+      erb :'pirates/show'
+    end
+    
   end
 end
